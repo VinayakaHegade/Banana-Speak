39e608813dde44a3a3f821559141045a037ba5ce
Vinayak <myselfvinayakhegde@gmail.com>
2021-08-23 22:41:16 +0530

    refactor

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var buttonTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var textOutput = document.querySelector("#text-output");
 
+function eventClickHandler() {
+    textOutput.innerText = textInput.value + "this is translated";
+}
 
-function eventClickHandler () {
-    textOutput.innerText = textInput.value+ "this is translated"
-};
-
-buttonTranslate.addEventListener("click", eventClickHandler)
+buttonTranslate.addEventListener("click", eventClickHandler);
